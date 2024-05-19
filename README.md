a644230a822a6be5c43e0254da4957d7a26dee66
miangeldev <miangeldev@proton.me>
2024-05-19 15:19:36 -0600

    0.0.21

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 PyKozo is a Python library that allows for complete web development using Python scripts. Currently, it only includes the complete HTML module, but other modules will be added in the future.
 
-Version 0.0.2
+Version 0.0.21
 
 Documentation coming soon...
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='PyKozo',
-    version='0.0.2',
+    version='0.0.21',
     license='MIT',
     description='PyKozo is a Python library that allows for complete web development using Python scripts.',
     author='Miguel Nevado',
@@ -11,5 +11,13 @@ setup(
     packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    keywords=['pykozo','html','tags','web','development']
+    keywords=['pykozo','html','tags','web','development'],
+    classifiers=[
+        'Development Status :: 2 - Prealpha',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+    ]
 )
