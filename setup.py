593ab576bb985352160302fd4648a9eb609e4ad1
miangeldev <miangeldev@proton.me>
2024-05-19 15:11:37 -0600

    update pypi setup

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup,find_packages
 
 setup(
     name='PyKozo',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='PyKozo is a Python library that allows for complete web development using Python scripts.',
     author='Miguel Nevado',
     author_email='miangeldev@proton.me',
     url='https://github.com/miangeldev/pykozo',
-    packages=find_packages()
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['pykozo','html','tags','web','development']
 )
