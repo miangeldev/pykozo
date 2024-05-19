8450a8d2e2ee6fee515cbf7e3d6f6406ddbe930b
miangeldev <miangeldev@proton.me>
2024-05-19 12:44:40 -0600

    setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='PyKozo',
+    version='0.0.1',
+    license='MIT',
+    description='PyKozo is a Python library that allows for complete web development using Python scripts.',
+    author='Miguel Nevado',
+    author_email='miangeldev@proton.me',
+    url='https://github.com/miangeldev/pykozo',
+    packages=find_packages()
+)
