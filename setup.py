1a666a6a0b6e369f6271e99027560e2e340bede1
miangeldev <miangeldev@proton.me>
2024-05-21 11:03:30 -0600

    start 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='PyKozo',
-    version='0.0.21',
+    version='0.0.3',
     license='MIT',
     description='PyKozo is a Python library that allows for complete web development using Python scripts.',
     author='Miguel Nevado',
